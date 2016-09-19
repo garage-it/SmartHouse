093d8d2ada9430759763931263fa957ffbe48ef3
Volodymyr Vreshch <vreshch@gmail.com>
2016-09-19 14:40:11 +0300

    [TG-TECH][chore] improve default env varibles

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Installation:
 * install mongodb & pm2
 * install all required dependencies
+* configure enviroment (If default configuration is not suitable)
 * run the script
 
 Install pm2 globally
@@ -35,3 +36,24 @@ To get status:
 ```sh
 $ npm run status
 ```
+
+## Configure enviroment :
+Via environment variables, by creating
+.evn file – otherwise default settings will apply
+
+### Mongo DB configuration:
+    * SH_MONGO_DB = 'mongodb://localhost/db'    
+
+### PATH to other libraries distribution:
+    * SH_PATH_FRONTENT_DIST = './node_modules/smart-house-frontend/dist'
+    * SH_PATH_BACKEND_CMD = './node_modules/smart-house-backend'
+    * SH_PATH_BROKER_CMD = './node_modules/smart-house-broker'
+
+### WEB Configuration:
+    * SH_WEB_PORT='80'
+
+### MQTT Configuration:
+    * SH_MQTT_PORT='1883'
+    * SH_MQTT_HOST_NAME='localhost'
+    * SH_MQTT_USER_NAME='USERNAME'
+    * SH_MQTT_PASSWORD='PASSWORD'
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const colors = require('colors');
 const fs = require('fs');
 const program = require('commander');
 const exec = require('shelljs').exec;
 const path = require('path');
+require('dotenv').config();
 
 /**
 * Init program version
@@ -18,17 +18,22 @@ program.version(packageJSON.version);
 const SH_PM2_BACKEND_NAME = 'smart-house-backend';
 const SH_PM2_BROKER_NAME = 'smart-house-broker';
 
-/**
-* Init path constants
-*/
-const SH_PATH_FRONTENT_DIST = path.resolve('./node_modules/smart-house-frontend/dist'); //'../SmartHouse-frontend/dist'
-const SH_PATH_BACKEND_CMD =  path.resolve('./node_modules/smart-house-backend'); //'../SmartHouse-backend'
-const SH_PATH_BROKER_CMD = path.resolve('./node_modules/smart-house-broker'); //'../SmartHouse-broker'
+// Mongo DB configuration
+let SH_MONGO_DB;
 
-/**
-* Will serve backend on specified port
-*/
-const SH_BACKEND_PORT = '8080';
+// PATH to other libraries distribution
+let SH_PATH_FRONTENT_DIST;
+let SH_PATH_BACKEND_CMD;
+let SH_PATH_BROKER_CMD;
+
+// WEB Configuration
+let SH_WEB_PORT;
+
+// MQTT Configuration
+let SH_MQTT_PORT;
+let SH_MQTT_HOST_NAME;
+let SH_MQTT_USER_NAME;
+let SH_MQTT_PASSWORD;
 
 program
     .command('start [port]')
@@ -37,16 +42,17 @@ program
     .action(startAction);
 
 function startAction(cmd, options) {
+    // Setup env varibles
+    InitEnvConfiguration();
+    process.env['ENV_CONFIG'] = 'production';
 
-    var port = SH_BACKEND_PORT;
+    var port = SH_WEB_PORT;
     if (program.args[0] && isFinite(parseInt(program.args[0])) ) {
         port = program.args[0];
     }
-    process.env['ENV_CONFIG'] = 'production';
-    process.env['SH_PATH_FRONTENT_DIST'] = SH_PATH_FRONTENT_DIST;
 
     // set port to serve the backend
-    process.env['SH_BACKEND_PORT'] = port;
+    process.env['SH_WEB_PORT'] = port;
 
     //start-backend
     exec('pm2 start ' + SH_PATH_BACKEND_CMD + ' --name ' + SH_PM2_BACKEND_NAME);
@@ -58,6 +64,35 @@ function startAction(cmd, options) {
         cmd += ' -- --mock';
     }
     exec(cmd);
+}
+
+function InitEnvConfiguration() {
+    // Mongo DB configuration
+    SH_MONGO_DB = process.env['SH_MONGO_DB'] || 'mongodb://localhost/db';
+
+    // PATH to other libraries distribution
+    SH_PATH_FRONTENT_DIST = process.env['SH_PATH_FRONTENT_DIST'] || path.resolve('./node_modules/smart-house-frontend/dist'); //'../SmartHouse-frontend/dist'
+    SH_PATH_BACKEND_CMD =  process.env['SH_PATH_BACKEND_CMD'] || path.resolve('./node_modules/smart-house-backend'); //'../SmartHouse-backend'
+    SH_PATH_BROKER_CMD = process.env['SH_PATH_BROKER_CMD'] || path.resolve('./node_modules/smart-house-broker'); //'../SmartHouse-broker'
+
+    // WEB Configuration
+    SH_WEB_PORT = process.env['SH_WEB_PORT'] || '8080';
+
+    // MQTT Configuration
+    SH_MQTT_PORT = process.env['SH_WEB_PORT'] || '1883';
+    SH_MQTT_HOST_NAME = process.env['SH_MQTT_HOST_NAME'] || 'localhost';
+    SH_MQTT_USER_NAME = process.env['SH_MQTT_USER_NAME'] || 'USERNAME';
+    SH_MQTT_PASSWORD = process.env ['SH_MQTT_PASSWORD']  || 'PASSWORD';
+
+
+    // init env
+    process.env['SH_MONGO_DB'] = SH_MONGO_DB;
+    process.env['SH_WEB_PORT'] = SH_WEB_PORT;
+
+    process.env['SH_WEB_PORT'] = SH_MQTT_PORT;
+    process.env['SH_MQTT_HOST_NAME'] = SH_MQTT_HOST_NAME;
+    process.env['SH_MQTT_USER_NAME'] = SH_MQTT_USER_NAME;
+    process.env ['SH_MQTT_PASSWORD']  = SH_MQTT_PASSWORD;
 }
 
 program
